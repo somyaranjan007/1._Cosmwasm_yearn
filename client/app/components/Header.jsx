da1ab6fc04ae64b1f6bcbc3d3936d826ae0123f7
chirag9899 <95237856+chirag9899@users.noreply.github.com>
2023-07-01 06:45:36 +0000

    updated

---
+++ client/app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { HeaderStyle } from "../styles/styles"
+
+const Header = () => {
+    return (
+        <div className = {HeaderStyle.mainDiv}>
+            Hello
+        </div>
+    )
+}
+
+export default Header
